bd1bc4784ea1c042219b069cc3afd4bd5c20101c
Matthew Guillen <matthewguillen777@gmail.com>
2021-04-21 22:37:52 -0400

    fixed homepage

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container">
-          <a className="navbar-brand" href="/" id="logo">Matthew Guillen</a>
+          <a className="navbar-brand" href="/ReactPortfolio/about" id="logo">Matthew Guillen</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
             aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
